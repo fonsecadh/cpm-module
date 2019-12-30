94c10a96c63567fea5f046037f5bc64994d48326
fonsecadh <UO258318@uniovi.es>
2019-12-30 18:43:05 +0100

    SigInWindow created. Mnemonics and stuff are needed.

---
+++ hugo_fonseca_53509642G/src/gui/credentials/users/SignInWindow.java
@@ -0,0 +1,103 @@
+package gui.credentials.users;
+
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class SignInWindow extends JDialog {
+
+	// Attributes
+	private JLabel lblUsername;
+	private JLabel lblSignIn;
+	private JTextField txtUsername;
+	private JLabel lblPasswd;
+	private JPasswordField pfPasswd;
+	private JButton btnSignIn;
+	private JLabel lblNewUser;
+	private JButton btnNewUser;
+
+	/**
+	 * Create the dialog.
+	 */
+	public SignInWindow() {
+		setTitle("Roulette: Sign In");
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new GridLayout(0, 1, 0, 0));
+		getContentPane().add(getLblSignIn());
+		getContentPane().add(getLblUsername());
+		getContentPane().add(getTxtUsername());
+		getContentPane().add(getLblPasswd());
+		getContentPane().add(getPfPasswd());
+		getContentPane().add(getBtnSignIn());
+		getContentPane().add(getLblNewUser());
+		getContentPane().add(getBtnNewUser());
+	}
+
+	private JLabel getLblUsername() {
+		if (lblUsername == null) {
+			lblUsername = new JLabel("User Name:");
+			lblUsername.setFont(new Font("Dialog", Font.BOLD, 14));
+		}
+		return lblUsername;
+	}
+
+	private JLabel getLblSignIn() {
+		if (lblSignIn == null) {
+			lblSignIn = new JLabel("Sign In");
+			lblSignIn.setFont(new Font("Dialog", Font.BOLD, 16));
+		}
+		return lblSignIn;
+	}
+
+	private JTextField getTxtUsername() {
+		if (txtUsername == null) {
+			txtUsername = new JTextField();
+			txtUsername.setFont(new Font("Dialog", Font.PLAIN, 14));
+			txtUsername.setColumns(10);
+		}
+		return txtUsername;
+	}
+
+	private JLabel getLblPasswd() {
+		if (lblPasswd == null) {
+			lblPasswd = new JLabel("Password:");
+			lblPasswd.setFont(new Font("Dialog", Font.BOLD, 14));
+		}
+		return lblPasswd;
+	}
+
+	private JPasswordField getPfPasswd() {
+		if (pfPasswd == null) {
+			pfPasswd = new JPasswordField();
+			pfPasswd.setFont(new Font("Dialog", Font.PLAIN, 14));
+		}
+		return pfPasswd;
+	}
+
+	private JButton getBtnSignIn() {
+		if (btnSignIn == null) {
+			btnSignIn = new JButton("Sign In");
+		}
+		return btnSignIn;
+	}
+
+	private JLabel getLblNewUser() {
+		if (lblNewUser == null) {
+			lblNewUser = new JLabel("New User");
+			lblNewUser.setFont(new Font("Dialog", Font.BOLD, 16));
+		}
+		return lblNewUser;
+	}
+
+	private JButton getBtnNewUser() {
+		if (btnNewUser == null) {
+			btnNewUser = new JButton("Sign Up");
+		}
+		return btnNewUser;
+	}
+}
